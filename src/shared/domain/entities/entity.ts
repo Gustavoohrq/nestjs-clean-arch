2b8b1a7adddf5c4a95318828b2e5772f34348059
Gustavoohrq <gustavofmariano@gmail.com>
2024-06-18 09:48:51 -0300

    test: teste unitario para classe abstrata

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,12 +1,12 @@
-import {v4 as uuidV4} from "uuid"
+import { v4 as uuidv4 } from 'uuid'
 
 export abstract class Entity<Props = any> {
-  public readonly _id: string;
+  public readonly _id: string
   public readonly props: Props
 
   constructor(props: Props, id?: string) {
-    this.props = props;
-    this._id = id || uuidV4()
+    this.props = props
+    this._id = id || uuidv4()
   }
 
   get id() {
@@ -16,7 +16,7 @@ export abstract class Entity<Props = any> {
   toJSON(): Required<{ id: string } & Props> {
     return {
       id: this._id,
-      ...this.props
+      ...this.props,
     } as Required<{ id: string } & Props>
   }
 }
