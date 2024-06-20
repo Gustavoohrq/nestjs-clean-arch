e48a1816149365edce4534d276876b74cbf8eb7a
Gustavoohrq <gustavofmariano@gmail.com>
2024-06-20 10:35:50 -0300

    chore: adding search method in memory

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -128,5 +128,6 @@ export class SearchResult<E extends Entity, Filter = string> {
   }
 }
 export interface SearchableRepositoryInterface<E extends Entity, Filter = string, SearchInput = SearchParams, SearchOutput = SearchResult<E, Filter>> extends RepositoryInterface<E> {
+  sortableFields: string[]
   search(props: SearchInput): Promise<SearchOutput>
 }
