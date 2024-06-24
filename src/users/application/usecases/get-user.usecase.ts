5c7aa32e1bdb4f7c05b3c5eef8fe9ae5afba8371
Gustavoohrq <gustavofmariano@gmail.com>
2024-06-24 11:37:12 -0300

    test: UsersController unit tests

--- src/users/application/usecases/get-user.usecase.ts
+++ src/users/application/usecases/get-user.usecase.ts
@@ -5,14 +5,14 @@ export namespace GetUserUseCase {
   export type Input = {
     id: string
   }
+  export type Output = UserOutput
 
-
-  export class UseCase implements DefaultUseCase<Input, UserOutput> {
+  export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(
       private userRepository: UserRepository.Repository
     ) { }
 
-    async execute(input: Input): Promise<UserOutput> {
+    async execute(input: Input): Promise<Output> {
       const entity = await this.userRepository.findById(input.id)
       return UserOutputMapper.toOutput(entity)
     }
