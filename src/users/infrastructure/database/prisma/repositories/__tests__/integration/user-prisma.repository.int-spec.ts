73e3e4db48e45dc2309e490117f490c61db347df
Gustavoohrq <gustavofmariano@gmail.com>
2024-06-25 11:46:45 -0300

    test: should apply only pagination when the other params are null

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -6,6 +6,8 @@ import { DatabaseModule } from "@/shared/infrastructure/database/database.module
 import { NotFoundError } from "@/shared/domain/errors/not-found-error"
 import { UserEntity } from "@/users/domain/entities/user.entity"
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder"
+import { UserRepository } from "@/users/domain/repositories/user.repository"
+import { SearchResult } from "@/shared/domain/repositories/searchable-repository-contracts"
 describe('UserPrismaRepository integrations tests', () => {
   const prismaService = new PrismaClient()
   let sut: UserPrismaRepository
@@ -52,5 +54,32 @@ describe('UserPrismaRepository integrations tests', () => {
     const entities = await sut.findAll()
     expect(entities).toHaveLength(1)
     entities.map(item => expect(item.toJSON().name).toStrictEqual(entity.toJSON().name))
+  })
+
+  describe('search method tests', () => {
+    it('should apply only pagination when the other params are null', async () => {
+      const createdAt = new Date()
+      const entities: UserEntity[] = []
+      const arrange = Array(16).fill(UserDataBuilder({}))
+      arrange.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...element,
+            name: `User${index}`,
+            email: `test${index}@mail.com`,
+            createdAt: new Date(createdAt.getTime() + index)
+          })
+        )
+      })
+      await prismaService.user.createMany({
+        data: entities.map(item => item.toJSON())
+      })
+
+      const searchOutput = await sut.search(new UserRepository.SearchParams())
+
+
+      expect(searchOutput).toBeInstanceOf(SearchResult)
+      expect(searchOutput.total).toBe(16)
+    })
   })
 })
