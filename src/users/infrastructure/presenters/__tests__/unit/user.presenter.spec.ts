add1d3f4a56c61bcd8453fbdc0c9d57aa4c8d46f
Gustavoohrq <gustavofmariano@gmail.com>
2024-06-26 15:41:03 -0300

    test: UserPresenter unit tests

---
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -0,0 +1,37 @@
+import { instanceToPlain } from 'class-transformer';
+import { UserPresenter } from '../../user.presenter';
+
+describe('UserPresenter unit tests', () => {
+  const createdAt = new Date()
+  let sut: UserPresenter;
+  let props = {
+    id: "587da622-3540-499e-9ab0-cd810e1e64c0",
+    name: 'test name',
+    email: 'a@a.com',
+    createdAt,
+    password: 'fake'
+
+  }
+  beforeEach(() => {
+    sut = new UserPresenter(props)
+  })
+
+  describe('constructor', () => {
+    it('should be defined', () => {
+      expect(sut.id).toEqual(props.id)
+      expect(sut.name).toEqual(props.name)
+      expect(sut.email).toEqual(props.email)
+      expect(sut.createdAt).toEqual(props.createdAt)
+    });
+  });
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut)
+    expect(output).toStrictEqual({
+      id: "587da622-3540-499e-9ab0-cd810e1e64c0",
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
+    })
+  });
+
+});
