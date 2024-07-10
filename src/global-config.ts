772c1460cf806be077cdce9ecc381b9c5bef3a92
Gustavo Henrique <gustavofmariano@gmail.com>
2024-07-10 13:34:27 -0300

    chore: Implementation of the ConflictErrorFilter exception filter

--- src/global-config.ts
+++ src/global-config.ts
@@ -2,16 +2,17 @@
 import { ClassSerializerInterceptor, INestApplication, ValidationPipe } from '@nestjs/common';
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
 import { Reflector } from '@nestjs/core';
+import { ConflictErrorFilter } from './shared/infrastructure/exception-filters/conflict-error/conflict-error.filter';
 
 export function applyGlobalConfig(app: INestApplication) {
-app.useGlobalPipes(
-  new ValidationPipe({
-    errorHttpStatusCode: 422,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true
-  })
-)
-  app.useGlobalInterceptors(new WrapperDataInterceptor( ),new ClassSerializerInterceptor(app.get(Reflector)))
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    })
+  )
+  app.useGlobalInterceptors(new WrapperDataInterceptor(), new ClassSerializerInterceptor(app.get(Reflector)))
+  app.useGlobalFilters(new ConflictErrorFilter())
 }
