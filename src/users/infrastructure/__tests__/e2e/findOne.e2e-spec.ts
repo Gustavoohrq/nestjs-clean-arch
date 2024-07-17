4e7e4c6ecc49fb718873014f936260afffd57556
Gustavo Henrique <gustavofmariano@gmail.com>
2024-07-17 13:02:37 -0300

    test: UserController e2e tests - findOne method

--- src/users/infrastructure/__tests__/e2e/findOne.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/findOne.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { INestApplication } from '@nestjs/common';
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
 import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from '../../users.module';
@@ -12,13 +12,11 @@ import { instanceToPlain } from 'class-transformer';
 import { applyGlobalConfig } from '@/global-config';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
-import { UpdateUserDto } from '../../dtos/update-user.dto';
 
 describe('UserController e2e tests', () => {
   let app: INestApplication
   let module: TestingModule
   let repository: UserRepository.Repository
-  let updateUserDto: UpdateUserDto
   const prismaService = new PrismaClient()
   let entity: UserEntity
 
@@ -34,45 +32,33 @@ describe('UserController e2e tests', () => {
   })
 
   beforeEach(async () => {
-    updateUserDto = {
-      name: 'test name'
-    }
     await prismaService.user.deleteMany()
     entity = new UserEntity(UserDataBuilder({}))
     await repository.insert(entity)
   })
-  describe('PUT /users/:id', () => {
-    it('should update a user', async () => {
-      updateUserDto.name = 'test name'
+  describe('GET /users/:id', () => {
+    it('should get a user', async () => {
       const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send(updateUserDto)
+        .get(`/users/${entity._id}`)
         .expect(200)
-      const user = await repository.findById(entity._id)
-      const presenter = UsersController.userToResponse(user.toJSON())
+      const presenter = UsersController.userToResponse(entity.toJSON())
       const serialized = instanceToPlain(presenter)
       expect(res.body.data).toStrictEqual(serialized)
     });
 
-    it('should return a error with 422 code when the request body is invalid', async () => {
-      const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send({})
-        .expect(422)
-      expect(res.body.error).toBe('Unprocessable Entity')
-      expect(res.body.message).toEqual([
-        'name should not be empty',
-        'name must be a string',
-      ])
-    })
 
     it('should return a error with 404 code when throw NotFoundError with invalid id.', async () => {
-      await request(app.getHttpServer())
-        .put(`/users/fakeId`)
-        .send(updateUserDto)
+      const res = await request(app.getHttpServer())
+        .get(`/users/fakeId`)
         .expect(404)
+        .expect({
+          statusCode: 404,
+          error: 'Not Found',
+          message: 'UserModel not found using ID fakeId'
+        })
+
     });
 
- 
+
   });
 });
